d93b3740d259a179ea81534e0312cd49c5250de1
Luciano Ratamero <luciano@ratamero.com>
2021-01-28 18:50:02 -0300

    Add stories for subcomponents

---
+++ apps/docs/stories/Card/subcomponents/DoubleAction.stories.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { DoubleAction as Component } from "../../../confetti-ds/src/Card";
+import { CardContext } from "../../../confetti-ds/src/Card/contexts";
+
+export default {
+  title: "Card/Subcomponents/Double Action",
+  component: Component,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile2",
+    },
+  },
+};
+
+export const DoubleAction = (args) => (
+  <CardContext.Provider value={{}}>
+    <Component {...args} />
+  </CardContext.Provider>
+);
+
+DoubleAction.args = {
+  actionsProps: [
+    {
+      text: "Action 1",
+      onClick: (e) => console.log("Action 1 clicked", e),
+      icon: "plus",
+    },
+    {
+      text: "Action 2",
+      onClick: (e) => console.log("Action 2 clicked", e),
+      icon: "minus",
+    },
+  ],
+};
