a2137181b67161eff2b13804f9cca5f70ed9e76b
Luciano Ratamero <luciano@ratamero.com>
2021-01-29 18:30:00 -0300

    Add a11y addon

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+  ],
+};
