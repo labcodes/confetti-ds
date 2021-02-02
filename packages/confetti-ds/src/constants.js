746fd6896cafc7f8d0318f9be917cddb721ebf09
Lucas Cavalcante <lucas@labodes.com.br>
2021-02-02 17:56:37 -0300

    Add 'ICON_COLORS' list to 'src/constants'

--- packages/confetti-ds/src/constants.js
+++ packages/confetti-ds/src/constants.js
@@ -36,3 +36,22 @@ export const ICON_TYPES = [
   "wallet",
   "workspace",
 ];
+
+export const ICON_COLORS = [
+  "white",
+  "black-75",
+  "mineral-10",
+  "mineral-20",
+  "mineral-30",
+  "mineral-40",
+  "mineral-60",
+  "mineral-70",
+  "mineral-80",
+  "mineral-90",
+  "teal-40",
+  "teal-60",
+  "teal-70",
+  "purple-40",
+  "purple-60",
+  "purple-70",
+];
