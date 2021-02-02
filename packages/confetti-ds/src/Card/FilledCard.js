ec20399c188e95d675fbc1680752f7dbf16ac6d6
Luciano Ratamero <luciano@ratamero.com>
2021-02-02 14:11:42 -0300

    Extract cards' prop types to remove storybook dependencies on docs

--- packages/confetti-ds/src/Card/FilledCard.js
+++ packages/confetti-ds/src/Card/FilledCard.js
@@ -1,15 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+
 import { CardContext } from "./contexts";
+import { filledCardChoices } from "./propTypes";
 
 export default class FilledCard extends React.Component {
   static propTypes = {
     /** Components that will be rendered in the Filled Card (CardImage, CardHeader, CardAction, etc.) */
     children: PropTypes.node.isRequired,
     /** Sets Filled Card's colors (mostly background colors) based on the chosen palette.  */
-    color: PropTypes.oneOf(["white", "mineral", "teal", "purple"]).isRequired,
+    color: PropTypes.oneOf(filledCardChoices.color).isRequired,
     /** Sets the skin of the rendered Filled Card. */
-    skin: PropTypes.oneOf(["pale", "vivid"]).isRequired,
+    skin: PropTypes.oneOf(filledCardChoices.skin).isRequired,
     /** Sets horizontal layout. It changes the position of CardImage. */
     isHorizontal: PropTypes.bool,
     /** Reduces paddings and margins on Card layout. */
