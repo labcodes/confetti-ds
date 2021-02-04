18e0673f8dafcd483202cd5c50e09e05b6fc1c8d
Lucas Cavalcante <lucas@labodes.com.br>
2021-02-04 19:30:00 -0300

    Add story for RemovableTag

---
+++ apps/docs/stories/Tag/RemovableTag.stories.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { TAG_ICONS, TAG_THUMBS, TAG_SKINS, TAG_COLORS } from "./constants";
+import { RemovableTag as Component } from "../../confetti-ds/src";
+
+export default {
+  title: "Tag/Removable Tag",
+  component: Component,
+  argTypes: {
+    thumbSrc: {
+      control: { type: "select", options: TAG_THUMBS },
+    },
+    icon: {
+      control: { type: "select", options: TAG_ICONS },
+    },
+    color: {
+      control: { type: "select", options: TAG_COLORS },
+    },
+    skin: {
+      control: { type: "select", options: TAG_SKINS },
+    },
+  },
+};
+
+export const RemovableTag = (args) => <Component {...args} />;
+RemovableTag.args = {
+  text: "demo tag",
+  onClick: () => alert("Tag removed"),
+};
