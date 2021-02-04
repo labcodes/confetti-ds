a1f07243b63e9e61fd2578742b9b6dec3d893f4b
Lucas Cavalcante <lucas@labodes.com.br>
2021-02-04 19:33:52 -0300

    Add story for TogglableTag

---
+++ apps/docs/stories/Tag/TogglableTag.stories.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { TAG_COLORS } from "./constants";
+import { TogglableTag as Component } from "../../confetti-ds/src";
+
+export default {
+  title: "Tag/Togglable Tag",
+  component: Component,
+  argTypes: {
+    color: {
+      control: { type: "select", options: TAG_COLORS },
+    },
+  },
+};
+
+export const TogglableTag = (args) => <Component {...args} />;
+TogglableTag.args = {
+  text: "demo tag",
+  onClick: () => alert("Tag toggled"),
+};
