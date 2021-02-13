24028fbadca689ffb7803b55a23dbbb69df0d8cc
Lucas Cavalcante <lucas@labodes.com.br>
2021-02-13 00:44:51 -0300

    Enable 'icon' prop to be selectable in 'Alert' and 'Banner'

--- src/Alert.js
+++ src/Alert.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { isUndefined } from "lodash";
+
 import Icon from "./Icon";
 import TextButton from "./Button/TextButton";
+import { ICON_TYPES } from "./constants";
 
 export default class Alert extends React.Component {
   static propTypes = {
@@ -11,7 +13,7 @@ export default class Alert extends React.Component {
     /** Type of the Alert. */
     type: PropTypes.oneOf(["info", "warn", "error"]),
     /** Sets the icon related to the alert’s message. */
-    icon: PropTypes.string.isRequired,
+    icon: PropTypes.oneOf(ICON_TYPES).isRequired,
     /** Adds props for buttons eg.: [example of how to use]. Check buttons page for more information. */
     buttonProps: PropTypes.shape({
       /** Alert's text button label. */
