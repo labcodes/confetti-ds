a9c6171d4161f58e90b10d3ea8b4878335352086
Lucas Cavalcante <lucas@labodes.com.br>
2021-02-22 19:32:26 -0300

    Create story for 'Simple Tag With Icon'

---
+++ apps/docs/stories/Tag/SimpleTagWithIcon.stories.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { ICON_TYPES, TAG_COLORS } from "../../confetti-ds/src/constants";
+import { SimpleTag as Component } from "../../confetti-ds/src";
+
+export default {
+  title: "Tag/Simple Tag With Icon",
+  component: Component,
+  argTypes: {
+    color: {
+      control: { type: "select", options: TAG_COLORS },
+    },
+    skin: {
+      control: { type: "inline-radio" },
+    },
+    icon: {
+      control: { type: "select", options: ICON_TYPES },
+    },
+    thumbSrc: {
+      control: false,
+    },
+  },
+};
+
+export const SimpleTagWithIcon = (args) => {
+  return (
+    <React.Fragment>
+      <Component {...args} />
+    </React.Fragment>
+  )
+};
+
+SimpleTagWithIcon.args = {
+  text: "demo tag",
+  icon: "coin",
+};
