48efb353e395660221e85081693d63eb7477c11e
Lucas Cavalcante <lucas@labodes.com.br>
2021-02-22 19:51:43 -0300

    Disable 'icon' control from 'DropdownTag' story

--- apps/docs/stories/Tag/DropdownTag.stories.js
+++ apps/docs/stories/Tag/DropdownTag.stories.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-import { ICON_TYPES, TAG_COLORS } from "../../confetti-ds/src/constants";
+import { TAG_COLORS } from "../../confetti-ds/src/constants";
 import { DropdownTag as Component  } from "../../confetti-ds/src";
 
 export default {
   title: "Tag/Dropdown Tag",
   component: Component,
   argTypes: {
-    icon: {
-      control: { type: "select", options: ICON_TYPES },
-    },
     color: {
       control: { type: "select", options: TAG_COLORS },
     },
     skin: {
       control: { type: "inline-radio" },
+    },
+    icon: {
+      control: false,
     },
   },
 };
