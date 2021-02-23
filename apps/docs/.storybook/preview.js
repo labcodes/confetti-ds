50de74a822681885057adccdebe8826c0ab7a81a
Lucas Cavalcante <lucas@labodes.com.br>
2021-02-23 13:27:40 -0300

    Add 'description' to Controls panel

--- apps/docs/.storybook/preview.js
+++ apps/docs/.storybook/preview.js
@@ -3,6 +3,7 @@ import "!style-loader!css-loader!sass-loader!./storybook-overrides.scss";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: { expanded: true },
   options: {
     storySort: {
       order: [
