e64f9d5b5464780a228f576c8adfcc7c5435b8c2
Lucas Cavalcante <lucas@labodes.com.br>
2021-02-24 14:30:25 -0300

    Remove unnecessary elements from 'sortStory'

--- apps/docs/.storybook/preview.js
+++ apps/docs/.storybook/preview.js
@@ -7,82 +7,15 @@ export const parameters = {
   options: {
     storySort: {
       order: [
-        'Elements', [
-          'Icon',
-          'Badge',
-        ],
-        'Actions', [
-          'Button', [
-            'Button',
-            'Outline Button',
-            'Text Button',
-          ],
-          'Toggle',
-        ],
-        'Content', [
-          'Card', [
-            'Filled Card',
-            'Outline Card',
-            'Outline Filled Card',
-            'With Double Action',
-            'With Link Action',
-            'With Overflowed Image',
-            'With Overlaid Header',
-            'Card Subcomponents', [
-              'Card Divider',
-              'Card Header',
-              'Card Image',
-              'Double Action',
-              'Link Action',
-            ],
-          ],
-          'Tag', [
-            'Simple Tag',
-            'Dropdown Tag',
-            'Removable Tag',
-          ],
-        ],
+        'Elements',
+        'Actions',
+        'Content',
         'Data Inputs', [
           'Checkbox',
           'Radio',
-          'Search', [
-            'Inline Search',
-            'Standard Search',
-          ],
-          'Text Input', [
-            'Text Input',
-            'Email Input',
-            'Password Input',
-          ]
         ],
-        'Navigation', [
-          'Sidebar', [
-            'Narrow',
-            'Narrow With Footer Avatar',
-            'Narrow Subcomponents', [
-              'Body',
-              'Collapse Button',
-              'Footer',
-              'Footer Button',
-              'Header',
-              'Item',
-              'Logotype',
-              'User Avatar',
-            ],
-          ],
-        ],
-        'Messaging', [
-          'Dialog', [
-            'Standard',
-            'Message',
-          ],
-          'System Messages', [
-            'Alert With Button',
-            'Alert Without Button',
-            'Banner With Button',
-            'Banner Without Button',
-          ],
-        ],
+        'Navigation',
+        'Messaging',
       ],
     },
   },
