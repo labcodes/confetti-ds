cb337028edc1ed74fd083b84d4ae03754d91da81
= <dco3@cin.ufpe.br>
2022-03-15 20:55:59 -0300

    feat: basic structure => Abstract, TagDropdown, propTypes, SectionTitle

---
+++ packages/confetti-ds/src/Dropdown/SectionTitle.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+
+export default class SectionTitle extends Component {
+  static propTypes = {
+    text: PropTypes.node.isRequired,
+    color: PropTypes.string,
+  };
+
+  static defaultProps = {
+    color: "teal",
+  };
+
+  render() {
+    const { text, color } = this.props;
+    return (
+      <span
+        className={`lab-dropdown__section-title lab-dropdown__section-title--color-${color}`}
+      >
+        {text}
+      </span>
+    );
+  }
+}
