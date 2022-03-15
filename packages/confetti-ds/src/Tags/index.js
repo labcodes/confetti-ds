7bb52a01d4d6f04f0197638cdf255809296ef336
= <dco3@cin.ufpe.br>
2022-03-15 19:23:37 -0300

    feat: created TagItem component

--- packages/confetti-ds/src/Tags/index.js
+++ packages/confetti-ds/src/Tags/index.js
@@ -2,3 +2,4 @@ export { default as SimpleTag } from "./SimpleTag";
 export { default as TogglableTag } from "./TogglableTag";
 export { default as RemovableTag } from "./RemovableTag";
 export { default as DropdownTag } from "./DropdownTag";
+export { default as TagItem } from "./TagItem";
