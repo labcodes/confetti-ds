205137866115ad63faf1640cf27e71578eb96fe7
= <dco3@cin.ufpe.br>
2022-03-15 21:29:04 -0300

    feat: add onClick event to open/close dropdown

--- src/Dropdown/TagDropdown.js
+++ src/Dropdown/TagDropdown.js
@@ -6,19 +6,28 @@ import AbstractDropdown from "./AbstractDropdown";
 export default class TagDropdown extends Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
-    text: PropTypes.string,
+    text: PropTypes.string.isRequired,
     color: PropTypes.string,
+    onOpen: PropTypes.func,
+    onClose: PropTypes.func,
   };
 
   static defaultProps = {
-    text: "foo",
     color: "teal",
+    onOpen: () => {},
+    onClose: () => {},
   };
 
   render() {
-    const { children, text, color } = this.props;
+    const { children, text, color, onOpen, onClose } = this.props;
     return (
-      <AbstractDropdown dropdownType="tag" text={text} color={color}>
+      <AbstractDropdown
+        dropdownType="tag"
+        text={text}
+        color={color}
+        onOpen={onOpen}
+        onClose={onClose}
+      >
         {children}
       </AbstractDropdown>
     );
