06ba7bba11db85de3f184e91391a4d2795f9c646
= <dco3@cin.ufpe.br>
2022-03-25 00:41:30 -0300

    feat: keyboard events for accessibility

--- src/Dropdown/propTypes.js
+++ src/Dropdown/propTypes.js
@@ -1,4 +1,12 @@
 export const dropdownOptions = {
   color: ["white", "mineral", "teal", "purple"],
-  types: ["tag", "select", "button"],
+  types: ["tag", "button"],
+};
+
+export const expectedKeys = {
+  ArrowDown: true,
+  ArrowUp: true,
+  PageDown: true,
+  PageUp: true,
+  Escape: true,
 };
