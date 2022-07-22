caef4ccc9a630690d94ca617e1dd06e45cb4df02
Juliana Barros Lima <judiembl@gmail.com>
2022-07-22 16:09:44 -0300

    reacftor: migrating CardDivider

--- src/Card/CardDivider.js
+++ src/Card/CardDivider.js
@@ -1,25 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default class CardDivider extends React.Component {
-  static propTypes = {
-    /** Sets whether the divider has the card's full width. */
-    isOverflowed: PropTypes.bool,
-  };
-
-  static defaultProps = {
-    isOverflowed: false,
-  };
-
-  render() {
-    const { isOverflowed } = this.props;
-
-    return (
-      <hr
-        className={`lab-card-divider
+export default function CardDivider({ isOverflowed }) {
+  return (
+    <hr
+      className={`lab-card-divider
           ${isOverflowed ? " lab-card-divider--overflowed" : ""}
         `}
-      />
-    );
-  }
+    />
+  );
 }
+
+CardDivider.propTypes = {
+  /** Sets whether the divider has the card's full width. */
+  isOverflowed: PropTypes.bool,
+};
+
+CardDivider.defaultProps = {
+  isOverflowed: false,
+};
