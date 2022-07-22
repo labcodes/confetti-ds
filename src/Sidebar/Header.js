f8c76868752a25378097e9b17467d2b8e8591cd4
= <dnc331@gmail.com>
2022-07-22 12:22:13 -0300

    Change Sidebar.Header from class to fn component

--- src/Sidebar/Header.js
+++ src/Sidebar/Header.js
@@ -1,18 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default class Header extends React.Component {
-  static propTypes = {
-    /** Components that will be rendered in the Header. */
-    children: PropTypes.node,
-  };
-
-  static defaultProps = {
-    children: undefined,
-  };
-
-  render() {
-    const { children } = this.props;
-    return <div className="lab-narrow-sidebar__header">{children}</div>;
-  }
+/**
+ *
+ * @param children Components that will be rendered in the Header.
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function Header({ children }) {
+  return <div className="lab-narrow-sidebar__header">{children}</div>;
 }
+
+Header.propTypes = {
+  /** Components that will be rendered in the Header. */
+  children: PropTypes.node,
+};
+
+Header.defaultProps = {
+  children: undefined,
+};
