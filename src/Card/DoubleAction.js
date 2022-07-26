4210f03d06eb9ba7df943ab3be78c1cb6a4cba2c
Juliana Barros Lima <judiembl@gmail.com>
2022-07-26 18:12:56 -0300

    refactor: migrating Double Action to Hooks

--- src/Card/DoubleAction.js
+++ src/Card/DoubleAction.js
@@ -1,55 +1,19 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 
 import { Button, OutlineButton, TextButton } from "../Button";
 import { CardContext } from "./contexts";
 
-export default class DoubleAction extends React.Component {
-  static propTypes = {
-    /** Sets button's attributes: label, action to be executed when the button is clicked, whether it is disabled/ariaDisabled or not, and the icon. */
-    actionsProps: PropTypes.arrayOf(
-      PropTypes.exact({
-        text: PropTypes.string.isRequired,
-        onClick: PropTypes.func.isRequired,
-        ariaDisabled: PropTypes.bool,
-        disabled: PropTypes.bool,
-        icon: PropTypes.string,
-      })
-    ).isRequired,
-    /** Sets buttons's height. Small = 32px, Normal = 40px, Large = 48px. */
-    size: PropTypes.oneOf(["normal", "small", "large"]),
-    /** Sets whether buttons will pile on top of each other or be on the same line. */
-    isHorizontal: PropTypes.bool,
-    /** Sets buttons to be Text Buttons. */
-    isText: PropTypes.bool,
-  };
-
-  static defaultProps = {
-    size: "normal",
-    isHorizontal: false,
-    isText: false,
-  };
-
-  constructor(props) {
-    super(props);
-    this.checkActionsPropsLength(props);
-  }
-
-  componentDidUpdate() {
-    this.checkActionsPropsLength(this.props);
-  }
-
-  checkActionsPropsLength = ({ actionsProps }) => {
-    if (actionsProps.length !== 2) {
-      throw Error(
-        "DoubleAction: You need to pass exactly two objects inside the `actionsProps` prop."
-      );
-    }
-  };
-
-  getButtonSkinFromCardContext = (context) => {
+export default function DoubleAction({
+  actionsProps,
+  size,
+  isText,
+  isHorizontal,
+}) {
+  const cardContext = useContext(CardContext);
+  const getButtonSkinFromCardContext = (context) => {
     const { color, skin, cardType } = context;
-
+    // console.log(context);
     if (cardType === "filled" && skin === "vivid" && color !== "white") {
       return "light";
     }
@@ -61,69 +25,102 @@ export default class DoubleAction extends React.Component {
     ) {
       return "dark";
     }
-
     return null;
   };
 
-  render() {
-    const { actionsProps, size, isText, isHorizontal } = this.props;
+  const checkActionsPropsLength = (actionsPropsList) => {
+    if (actionsPropsList.length !== 2) {
+      throw Error(
+        "DoubleAction: You need to pass exactly two objects inside the `actionsProps` prop."
+      );
+    }
+  };
 
-    return (
-      <CardContext.Consumer>
-        {(context) => (
-          <section
-            className={`lab-card-double-action lab-card-double-action--${size}
-              ${isText ? " lab-card-double-action--text" : ""}
-              ${isHorizontal ? " lab-card-double-action--horizontal" : ""}
-            `}
-          >
-            {actionsProps.map(
-              ({ text, onClick, icon, disabled, ariaDisabled }, index) => {
-                if (isText) {
-                  return (
-                    <TextButton
-                      key={text}
-                      text={text}
-                      onClick={onClick}
-                      size={size}
-                      skin={this.getButtonSkinFromCardContext(context)}
-                      {...(icon ? { icon } : undefined)}
-                      {...(disabled ? { disabled } : undefined)}
-                      {...(ariaDisabled ? { ariaDisabled } : undefined)}
-                    />
-                  );
-                }
-                if (index === 0) {
-                  return (
-                    <Button
-                      key={text}
-                      text={text}
-                      onClick={onClick}
-                      size={size}
-                      skin={this.getButtonSkinFromCardContext(context)}
-                      {...(icon ? { icon } : undefined)}
-                      {...(disabled ? { disabled } : undefined)}
-                      {...(ariaDisabled ? { ariaDisabled } : undefined)}
-                    />
-                  );
-                }
-                return (
-                  <OutlineButton
-                    key={text}
-                    text={text}
-                    onClick={onClick}
-                    size={size}
-                    skin={this.getButtonSkinFromCardContext(context)}
-                    {...(icon ? { icon } : undefined)}
-                    {...(disabled ? { disabled } : undefined)}
-                    {...(ariaDisabled ? { ariaDisabled } : undefined)}
-                  />
-                );
-              }
-            )}
-          </section>
-        )}
-      </CardContext.Consumer>
-    );
-  }
+  useEffect(() => {
+    checkActionsPropsLength(actionsProps);
+  }, [actionsProps]);
+
+  return (
+    // <CardContext.Consumer>
+    //   {({ color, skin, cardType }) => (
+    <section
+      className={`lab-card-double-action lab-card-double-action--${size}
+            ${isText ? " lab-card-double-action--text" : ""}
+            ${isHorizontal ? " lab-card-double-action--horizontal" : ""}
+          `}
+    >
+      {actionsProps.map(
+        ({ text, onClick, icon, disabled, ariaDisabled }, index) => {
+          if (isText) {
+            return (
+              <TextButton
+                key={text}
+                text={text}
+                onClick={onClick}
+                size={size}
+                //  todo: reescrever para getContext
+                skin={getButtonSkinFromCardContext(cardContext)}
+                {...(icon ? { icon } : undefined)}
+                {...(disabled ? { disabled } : undefined)}
+                {...(ariaDisabled ? { ariaDisabled } : undefined)}
+              />
+            );
+          }
+          if (index === 0) {
+            return (
+              <Button
+                key={text}
+                text={text}
+                onClick={onClick}
+                size={size}
+                skin={getButtonSkinFromCardContext(cardContext)}
+                {...(icon ? { icon } : undefined)}
+                {...(disabled ? { disabled } : undefined)}
+                {...(ariaDisabled ? { ariaDisabled } : undefined)}
+              />
+            );
+          }
+          return (
+            <OutlineButton
+              key={text}
+              text={text}
+              onClick={onClick}
+              size={size}
+              skin={getButtonSkinFromCardContext(cardContext)}
+              {...(icon ? { icon } : undefined)}
+              {...(disabled ? { disabled } : undefined)}
+              {...(ariaDisabled ? { ariaDisabled } : undefined)}
+            />
+          );
+        }
+      )}
+    </section>
+    //   )}
+    // </CardContext.Consumer>
+  );
 }
+
+DoubleAction.propTypes = {
+  /** Sets button's attributes: label, action to be executed when the button is clicked, whether it is disabled/ariaDisabled or not, and the icon. */
+  actionsProps: PropTypes.arrayOf(
+    PropTypes.exact({
+      text: PropTypes.string.isRequired,
+      onClick: PropTypes.func.isRequired,
+      ariaDisabled: PropTypes.bool,
+      disabled: PropTypes.bool,
+      icon: PropTypes.string,
+    })
+  ).isRequired,
+  /** Sets buttons's height. Small = 32px, Normal = 40px, Large = 48px. */
+  size: PropTypes.oneOf(["normal", "small", "large"]),
+  /** Sets whether buttons will pile on top of each other or be on the same line. */
+  isHorizontal: PropTypes.bool,
+  /** Sets buttons to be Text Buttons. */
+  isText: PropTypes.bool,
+};
+
+DoubleAction.defaultProps = {
+  size: "normal",
+  isHorizontal: false,
+  isText: false,
+};
