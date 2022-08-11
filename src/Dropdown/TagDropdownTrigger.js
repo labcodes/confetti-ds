94a7be5e582e7b3d01db1c451fb6c304d78f4243
Juliana Barros Lima <judiembl@gmail.com>
2022-08-11 13:00:04 -0300

    refactor: migrating TagDropdownTriger due its dependence from Abstract Tag

--- src/Dropdown/TagDropdownTrigger.js
+++ src/Dropdown/TagDropdownTrigger.js
@@ -1,48 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 import AbstractTag from "../Tags/AbstractTag";
 import { DropdownTag } from "../Tags";
 
-export default class TagDropdownTrigger extends DropdownTag {
-  constructor(props) {
-    super(props);
-
-    this.ref = React.createRef();
-  }
-
-  render() {
-    const {
-      text,
-      icon,
-      color,
-      skin,
-      isOutline,
-      disabled,
-      ariaDisabled,
-      onInteraction,
-      tabIndex,
-      setRef,
-    } = this.props;
-
-    return (
-      <AbstractTag
-        className={`lab-tag--dropdown${`${
-          icon ? ` lab-tag--has-left-icon` : ""
-        }`}`}
-        text={text}
-        icon={icon}
-        color={color}
-        skin={skin}
-        isOutline={isOutline}
-        disabled={!ariaDisabled && disabled}
-        ariaDisabled={ariaDisabled}
-        renderPrefix={this.icon()}
-        renderSuffix={this.dropdownIcon()}
-        tabIndex={tabIndex}
-        onInteraction={onInteraction}
-        isDropdown
-        setRef={setRef}
-        role="listbox"
-      />
-    );
-  }
+export default function TagDropdownTrigger({
+  text,
+  icon,
+  color,
+  skin,
+  isOutline,
+  disabled,
+  ariaDisabled,
+  onInteraction,
+  tabIndex,
+  dropdownIcon,
+  handleIcon
+}) {
+  const ref = useRef();
+  return (
+    <AbstractTag
+      className={`lab-tag--dropdown${`${
+        icon ? ` lab-tag--has-left-icon` : ""
+      }`}`}
+      text={text}
+      icon={icon}
+      color={color}
+      skin={skin}
+      isOutline={isOutline}
+      disabled={!ariaDisabled && disabled}
+      ariaDisabled={ariaDisabled}
+      renderPrefix={handleIcon()}
+      renderSuffix={dropdownIcon()}
+      tabIndex={tabIndex}
+      onInteraction={onInteraction}
+      isDropdown
+      setRef={ref}
+      role="listbox"
+    />
+  );
 }
