e64ea70d339280736192032b492b2a9f6b4c7e5f
Juliana Barros Lima <judiembl@gmail.com>
2022-08-15 16:04:41 -0300

    refactor: migrating Alert to function component

--- src/Alert.js
+++ src/Alert.js
@@ -6,65 +6,53 @@ import Icon from "./Icon";
 import TextButton from "./Button/TextButton";
 import { ICON_TYPES } from "./constants";
 
-export default class Alert extends React.Component {
-  static propTypes = {
-    /** This is the message text string. */
-    text: PropTypes.string.isRequired,
-    /** Type of the Alert. */
-    type: PropTypes.oneOf(["info", "warn", "error"]),
-    /** Sets the icon related to the alert’s message. */
-    icon: PropTypes.oneOf(ICON_TYPES).isRequired,
-    /** Adds props for buttons eg.: [example of how to use]. Check buttons page for more information. */
-    buttonProps: PropTypes.shape({
-      /** Alert's text button label. */
-      text: PropTypes.string,
-      /** Action to be executed when the button is clicked. */
-      onClick: PropTypes.func,
-    }),
-  };
-
-  static defaultProps = {
-    type: "info",
-    buttonProps: { text: "", onClick: () => {} },
-  };
-
-  icon = () => {
-    const { icon } = this.props;
-    return icon ? (
+export default function Alert({ text, type, icon, buttonProps }) {
+  const handleIcon = () =>
+    icon ? (
       <Icon type={icon} color="mineral-70" className="lab-alert__icon" />
     ) : undefined;
-  };
 
-  button = () => {
-    const {
-      buttonProps: { text },
-    } = this.props;
-    return text ? (
-      <TextButton size="normal" skin="dark" text={text} />
-    ) : undefined;
-  };
+  const button = () =>
+    text ? <TextButton size="normal" skin="dark" text={text} /> : undefined;
 
-  handleClick = (event) => {
-    const { buttonProps } = this.props;
+  const handleClick = (event) => {
     if (!isUndefined(buttonProps.onClick)) {
       buttonProps.onClick(event);
     }
   };
 
-  render() {
-    const { text, type } = this.props;
-    return (
-      <div className={`lab-alert lab-alert--${type}`}>
-        {this.icon()}
-        <span className="lab-alert__message">{text}</span>
-        <span
-          className="lab-alert__button"
-          onClick={this.handleClick}
-          role="presentation"
-        >
-          {this.button()}
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className={`lab-alert lab-alert--${type}`}>
+      {handleIcon()}
+      <span className="lab-alert__message">{text}</span>
+      <span
+        className="lab-alert__button"
+        onClick={handleClick}
+        role="presentation"
+      >
+        {button()}
+      </span>
+    </div>
+  );
 }
+
+Alert.propTypes = {
+  /** This is the message text string. */
+  text: PropTypes.string.isRequired,
+  /** Type of the Alert. */
+  type: PropTypes.oneOf(["info", "warn", "error"]),
+  /** Sets the icon related to the alert’s message. */
+  icon: PropTypes.oneOf(ICON_TYPES).isRequired,
+  /** Adds props for buttons e.g.: [example of how to use]. Check buttons page for more information. */
+  buttonProps: PropTypes.shape({
+    /** Alert's text button label. */
+    text: PropTypes.string,
+    /** Action to be executed when the button is clicked. */
+    onClick: PropTypes.func,
+  }),
+};
+
+Alert.defaultProps = {
+  type: "info",
+  buttonProps: { text: "", onClick: () => {} },
+};
