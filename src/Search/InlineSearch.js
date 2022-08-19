cc98ce1b87533d39277573f045298ef2ede79342
Juliana Barros Lima <12106738+tiidadavena@users.noreply.github.com>
2022-08-19 15:39:08 -0300

    [CSYS-316] Migrate Search components (#37)
    * fix: recovering files from merge
    
    * fix: updating state reload
    
    * fix: updating onClear func
    
    * test: updating Search tests snapshots
    
    * fix: removing unused code from Inline Search
    
    * fix: minor code fixes
    
    * docs: adding JS Docs to the Search Components
    
    * fix: uploading new tests' snapshots
    
    * fix: removing unused code

--- src/Search/InlineSearch.js
+++ src/Search/InlineSearch.js
@@ -3,68 +3,77 @@ import PropTypes from "prop-types";
 
 import AbstractSearch from "./AbstractSearch";
 
-export default class InlineSearch extends React.Component {
-  static propTypes = {
-    /** Text that will serve as unique identifier. It's also an important accessibility tool. */
-    id: PropTypes.string,
-    /** Defines a default value for the Search initialization. */
-    defaultValue: PropTypes.string,
-    /** Disables the Search. Will be read by screen readers. When true, will override `disabled`. */
-    ariaDisabled: PropTypes.bool,
-    /** Disables the Search. Won't be read by screen readers. */
-    disabled: PropTypes.bool,
-    /** Text that will be rendered inside the Input field. */
-    value: PropTypes.string,
-    /** Action to be executed when the Search default value changes. */
-    onChange: PropTypes.func,
-    /** Action to be executed when the search is performed. */
-    onSearch: PropTypes.func,
-    /** Action to be executed when the Search is cleared out. */
-    onClear: PropTypes.func,
-    /** The placeholder text when the Search field is empty. Usually used to describe the values accepted (e.g.: Search by keyword or status). */
-    placeholder: PropTypes.string,
-  };
-
-  static defaultProps = {
-    id: undefined,
-    defaultValue: undefined,
-    disabled: false,
-    ariaDisabled: false,
-    value: undefined,
-    onChange: () => {},
-    onSearch: () => {},
-    onClear: () => {},
-    placeholder: "Search",
-  };
-
-  render() {
-    const {
-      id,
-      defaultValue,
-      disabled,
-      ariaDisabled,
-      value,
-      onChange,
-      onSearch,
-      onClear,
-      placeholder,
-    } = this.props;
-
-    return (
-      <div className="lab-inline-search">
-        <AbstractSearch
-          type="inline"
-          id={id}
-          defaultValue={defaultValue}
-          value={value}
-          onChange={onChange}
-          onSearch={onSearch}
-          onClear={onClear}
-          placeholder={placeholder}
-          disabled={(!ariaDisabled && disabled) || undefined}
-          ariaDisabled={ariaDisabled}
-        />
-      </div>
-    );
-  }
+/**
+ * @param id   is the text that will serve as unique identifier. It's also an important accessibility tool.
+ * @param defaultValue defines a default value for the Search initialization.
+ * @param disabled disables the Search. Won't be read by screen readers.
+ * @param ariaDisabled disables the Search. Will be read by screen readers. When true, will override `disabled`.
+ * @param value is the text that will be rendered inside the Search field.
+ * @param onChange  it's an action  to be executed when the Search default value changes.
+ * @param onSearch it's an action to be executed when the search is performed.
+ * @param onClear it's an action to be executed when the Search field is cleared out.
+ * @param placeholder it's the placeholder text when the Search field is empty. Usually used to describe the values accepted (e.g.: Search by keyword or status).
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function InlineSearch({
+  id,
+  defaultValue,
+  disabled,
+  ariaDisabled,
+  value,
+  onChange,
+  onSearch,
+  onClear,
+  placeholder,
+}) {
+  return (
+    <div className="lab-inline-search">
+      <AbstractSearch
+        type="inline"
+        id={id}
+        defaultValue={defaultValue}
+        value={value}
+        onChange={onChange}
+        onSearch={onSearch}
+        onClear={onClear}
+        placeholder={placeholder}
+        disabled={(!ariaDisabled && disabled) || undefined}
+        ariaDisabled={ariaDisabled}
+      />
+    </div>
+  );
 }
+
+InlineSearch.propTypes = {
+  /** Text that will serve as unique identifier. It's also an important accessibility tool. */
+  id: PropTypes.string,
+  /** Defines a default value for the Search initialization. */
+  defaultValue: PropTypes.string,
+  /** Disables the Search. Will be read by screen readers. When true, will override `disabled`. */
+  ariaDisabled: PropTypes.bool,
+  /** Disables the Search. Won't be read by screen readers. */
+  disabled: PropTypes.bool,
+  /** Text that will be rendered inside the Input field. */
+  value: PropTypes.string,
+  /** Action to be executed when the Search default value changes. */
+  onChange: PropTypes.func,
+  /** Action to be executed when the search is performed. */
+  onSearch: PropTypes.func,
+  /** Action to be executed when the Search is cleared out. */
+  onClear: PropTypes.func,
+  /** The placeholder text when the Search field is empty. Usually used to describe the values accepted (e.g.: Search by keyword or status). */
+  placeholder: PropTypes.string,
+};
+
+InlineSearch.defaultProps = {
+  id: undefined,
+  defaultValue: undefined,
+  disabled: false,
+  ariaDisabled: false,
+  value: undefined,
+  onChange: () => {},
+  onSearch: () => {},
+  onClear: () => {},
+  placeholder: "Search",
+};
--- src/Search/StandardSearch.js
+++ src/Search/StandardSearch.js
@@ -3,68 +3,78 @@ import PropTypes from "prop-types";
 
 import AbstractSearch from "./AbstractSearch";
 
-export default class StandardSearch extends React.Component {
-  static propTypes = {
-    /** Text that will serve as unique identifier. It's also an important accessibility tool. */
-    id: PropTypes.string,
-    /** Defines a default value for the Search initialization. */
-    defaultValue: PropTypes.string,
-    /** Disables the Search. Will be read by screen readers. When true, will override `disabled`. */
-    ariaDisabled: PropTypes.bool,
-    /** Disables the Search. Won't be read by screen readers. */
-    disabled: PropTypes.bool,
-    /** Text that will be rendered inside the Search field. */
-    value: PropTypes.string,
-    /** Action to be executed when the Search default value changes. */
-    onChange: PropTypes.func,
-    /** Action to be executed when the search is performed. */
-    onSearch: PropTypes.func,
-    /** Action to be executed when the Search is cleared out. */
-    onClear: PropTypes.func,
-    /** The placeholder text when the Search field is empty. Usually used to describe the values accepted (e.g.: Search by keyword or status). */
-    placeholder: PropTypes.string,
-  };
+/**
+ * @param id  is the text that will serve as unique identifier. It's also an important accessibility tool.
+ * @param defaultValue defines a default value for the Search initialization.
+ * @param disabled disables the Search. Won't be read by screen readers.
+ * @param ariaDisabled  disables the Search. Will be read by screen readers. When true, will override `disabled`.
+ * @param value is the text that will be rendered inside the Search field.
+ * @param onChange it's an action  to be executed when the Search default value changes.
+ * @param onSearch it's an action to be executed when the search is performed.
+ * @param onClear  it's an action to be executed when the Search field is cleared out.
+ * @param placeholder  it's the placeholder text when the Search field is empty. Usually used to describe the values accepted (e.g.: Search by keyword or status).
+ * @returns {JSX.Element}
+ * @constructor
+ */
 
-  static defaultProps = {
-    id: undefined,
-    defaultValue: undefined,
-    disabled: false,
-    ariaDisabled: false,
-    value: undefined,
-    onChange: () => {},
-    onSearch: () => {},
-    onClear: () => {},
-    placeholder: "Search",
-  };
-
-  render() {
-    const {
-      id,
-      defaultValue,
-      disabled,
-      ariaDisabled,
-      value,
-      onChange,
-      onSearch,
-      onClear,
-      placeholder,
-    } = this.props;
-
-    return (
-      <div className="lab-standard-search">
-        <AbstractSearch
-          type="standard"
-          id={id}
-          defaultValue={defaultValue}
-          value={value}
-          onChange={onChange}
-          onSearch={onSearch}
-          onClear={onClear}
-          placeholder={placeholder}
-          disabled={(!ariaDisabled && disabled) || undefined}
-          ariaDisabled={ariaDisabled}
-        />
-      </div>
-    );
-  }
+export default function StandardSearch({
+  id,
+  defaultValue,
+  disabled,
+  ariaDisabled,
+  value,
+  onChange,
+  onSearch,
+  onClear,
+  placeholder,
+}) {
+  return (
+    <div className="lab-standard-search">
+      <AbstractSearch
+        type="standard"
+        id={id}
+        defaultValue={defaultValue}
+        value={value}
+        onChange={onChange}
+        onSearch={onSearch}
+        onClear={onClear}
+        placeholder={placeholder}
+        disabled={(!ariaDisabled && disabled) || undefined}
+        ariaDisabled={ariaDisabled}
+      />
+    </div>
+  );
 }
+
+StandardSearch.propTypes = {
+  /** Text that will serve as unique identifier. It's also an important accessibility tool. */
+  id: PropTypes.string,
+  /** Defines a default value for the Search initialization. */
+  defaultValue: PropTypes.string,
+  /** Disables the Search. Will be read by screen readers. When true, will override `disabled`. */
+  ariaDisabled: PropTypes.bool,
+  /** Disables the Search. Won't be read by screen readers. */
+  disabled: PropTypes.bool,
+  /** Text that will be rendered inside the Search field. */
+  value: PropTypes.string,
+  /** Action to be executed when the Search default value changes. */
+  onChange: PropTypes.func,
+  /** Action to be executed when the search is performed. */
+  onSearch: PropTypes.func,
+  /** Action to be executed when the Search is cleared out. */
+  onClear: PropTypes.func,
+  /** The placeholder text when the Search field is empty. Usually used to describe the values accepted (e.g.: Search by keyword or status). */
+  placeholder: PropTypes.string,
+};
+
+StandardSearch.defaultProps = {
+  id: undefined,
+  defaultValue: undefined,
+  disabled: false,
+  ariaDisabled: false,
+  value: undefined,
+  onChange: () => {},
+  onSearch: () => {},
+  onClear: () => {},
+  placeholder: "Search",
+};
