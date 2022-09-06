e40c61fce46e34d07cbf2312d689ee1b3d8107c5
Juliana Barros Lima <12106738+tiidadavena@users.noreply.github.com>
2022-09-06 11:11:08 -0300

    [CSYS-318] Migrating Tag components (#39)
    * refactor: migrating Abstract Dropdown
    
    * refactor: migrating Dropdown Tag to Hooks
    
    * fix: removing unused code
    
    * refactor: migrating RemovableTag to Hooks
    
    * docs: adding JS Docs to the Removable Tag
    
    * refactor: migrating Simple Tag to Hooks
    
    * refactor: migrating Simple Tag to Hooks
    
    * refactor: migrating Togglable Tag to Hooks
    
    * fix: solving lint issues
    
    * fix: removing deprecated event listeners
    
    * fix: removing unused code
    
    * docs: adding JS docs to the component
    
    * fix: removing unused code
    
    * refactor: migrating TagDropdownTriger due its dependence from Abstract Tag
    
    * refactor: fixing props validation
    
    * fix: removing unused code
    
    * fix: passing the setRef func to Tag subcomponents
    
    * docs: removing JS Docs
    
    * fix: minor fixes at className attribute from the AbstractTag import

--- src/Tags/TogglableTag.js
+++ src/Tags/TogglableTag.js
@@ -5,35 +5,16 @@ import AbstractTag from "./AbstractTag";
 import Icon from "../Icon";
 import { TAG_COLORS } from "../constants";
 
-export default class TogglableTag extends React.Component {
-  static propTypes = {
-    /** This is the Tag's text. */
-    text: PropTypes.string.isRequired,
-    /** Sets Tag's color. */
-    color: PropTypes.oneOf(TAG_COLORS),
-    /** Sets an outline style. */
-    isOutline: PropTypes.bool,
-    /** Disables the Tag. Will be read by screen readers. When true, will override `disabled`. */
-    ariaDisabled: PropTypes.bool,
-    /** Disables the Tag. Won't be read by screen readers. */
-    disabled: PropTypes.bool,
-    /** Action to be executed when the Tag is clicked. */
-    onClick: PropTypes.func,
-    /** Defines if the Tag is toggled on. */
-    isOn: PropTypes.bool,
-  };
-
-  static defaultProps = {
-    color: undefined,
-    isOutline: false,
-    disabled: false,
-    ariaDisabled: false,
-    isOn: false,
-    onClick: () => {},
-  };
-
-  selected = () => {
-    const { isOn } = this.props;
+export default function TogglableTag({
+  text,
+  color,
+  isOutline,
+  disabled,
+  ariaDisabled,
+  isOn,
+  onClick,
+}) {
+  const selected = () => {
     let iconClass;
 
     if (isOn) {
@@ -46,23 +27,44 @@ export default class TogglableTag extends React.Component {
       <Icon type="check" color="black-75" size="petit" className={iconClass} />
     );
   };
-
-  render() {
-    const { text, color, isOutline, disabled, ariaDisabled, isOn, onClick } =
-      this.props;
-    return (
-      <AbstractTag
-        className={`lab-tag--togglable${isOn ? " lab-tag--selected" : ""}`}
-        text={text}
-        color={color}
-        isOutline={isOutline}
-        disabled={!ariaDisabled && disabled}
-        ariaDisabled={ariaDisabled}
-        onClick={onClick}
-        skin={isOn ? "vivid" : "pale"}
-        renderPrefix={this.selected()}
-        tabIndex="0"
-      />
-    );
-  }
+  return (
+    <AbstractTag
+      className={`lab-tag--togglable${isOn ? " lab-tag--selected" : ""}`}
+      text={text}
+      color={color}
+      isOutline={isOutline}
+      disabled={!ariaDisabled && disabled}
+      ariaDisabled={ariaDisabled}
+      onClick={onClick}
+      skin={isOn ? "vivid" : "pale"}
+      renderPrefix={selected()}
+      tabIndex="0"
+    />
+  );
 }
+
+TogglableTag.propTypes = {
+  /** This is the Tag's text. */
+  text: PropTypes.string.isRequired,
+  /** Sets Tag's color. */
+  color: PropTypes.oneOf(TAG_COLORS),
+  /** Sets an outline style. */
+  isOutline: PropTypes.bool,
+  /** Disables the Tag. Will be read by screen readers. When true, will override `disabled`. */
+  ariaDisabled: PropTypes.bool,
+  /** Disables the Tag. Won't be read by screen readers. */
+  disabled: PropTypes.bool,
+  /** Action to be executed when the Tag is clicked. */
+  onClick: PropTypes.func,
+  /** Defines if the Tag is toggled on. */
+  isOn: PropTypes.bool,
+};
+
+TogglableTag.defaultProps = {
+  color: undefined,
+  isOutline: false,
+  disabled: false,
+  ariaDisabled: false,
+  isOn: false,
+  onClick: () => {},
+};
