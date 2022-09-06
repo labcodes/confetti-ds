c9d24639d4523ff0e71122dde626ee0d9e80a8e4
Juliana Barros Lima <12106738+tiidadavena@users.noreply.github.com>
2022-09-06 11:11:08 -0300

    [CSYS-318] Migrating Tag components (#39)
    * refactor: migrating Abstract Dropdown
    
    * refactor: migrating Dropdown Tag to Hooks
    
    * fix: removing unused code
    
    * refactor: migrating RemovableTag to Hooks
    
    * docs: adding JS Docs to the Removable Tag
    
    * refactor: migrating Simple Tag to Hooks
    
    * refactor: migrating Simple Tag to Hooks
    
    * refactor: migrating Togglable Tag to Hooks
    
    * fix: solving lint issues
    
    * fix: removing deprecated event listeners
    
    * fix: removing unused code
    
    * docs: adding JS docs to the component
    
    * fix: removing unused code
    
    * refactor: migrating TagDropdownTriger due its dependence from Abstract Tag
    
    * refactor: fixing props validation
    
    * fix: removing unused code
    
    * fix: passing the setRef func to Tag subcomponents
    
    * docs: removing JS Docs
    
    * fix: minor fixes at className attribute from the AbstractTag import

--- packages/confetti-ds/src/Tags/AbstractTag.js
+++ packages/confetti-ds/src/Tags/AbstractTag.js
@@ -1,79 +1,30 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import { isUndefined } from "lodash";
 
-export default class AbstractTag extends React.Component {
-  static propTypes = {
-    /** Tag's left element (optional and customizable) */
-    renderPrefix: PropTypes.object,
-    /** Components to be rendered at the end of the Tag. */
-    renderSuffix: PropTypes.object,
-    /** Defines a class name to create a custom style for the component. */
-    className: PropTypes.string,
-    /** This is the Tag's text. */
-    text: PropTypes.string.isRequired,
-    /** Sets Tag's color. */
-    color: PropTypes.string,
-    /** Skin of the the rendered Tag. */
-    skin: PropTypes.string,
-    /** Sets an outline style. */
-    isOutline: PropTypes.bool,
-    /** Disables the Tag. Will be read by screen readers. When true, will override `disabled`. */
-    ariaDisabled: PropTypes.bool,
-    /** Disables the Tag. Won't be read by screen readers. */
-    disabled: PropTypes.bool,
-    /** Action to be executed when the Tag is clicked. */
-    onClick: PropTypes.func,
-    /** tabIndex is used to define the navigation order for focusable elements. If not undefined, it is passed to the tag component. */
-    tabIndex: PropTypes.string,
-    /** This prop is a boolean to verify if the tag is a child of the TagDropdown component */
-    isDropdown: PropTypes.bool,
-    /** This prop sets the TagDropdownItem ref for option and triggers in a TagDropdown component. */
-    setRef: PropTypes.func,
-    /** This function is used to handle click or keydown interactions */
-    onInteraction: PropTypes.func,
-    /** This is the TagDropdownItem value. It is used TagDropdown options */
-    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    /**  This prop is used to verify if the option is selected */
-    isSelected: PropTypes.bool,
-    /**   This is the TagDropdownItem or TagDropdownTrigger role. It is used to let disabled people know the type of the element. */
-    role: PropTypes.string,
-  };
+export default function AbstractTag({
+  text,
+  disabled,
+  ariaDisabled,
+  isOutline,
+  skin,
+  color,
+  renderPrefix,
+  renderSuffix,
+  className,
+  tabIndex,
+  isDropdown,
+  value,
+  isSelected,
+  role,
+  onClick,
+  onInteraction,
+  setRef,
+}) {
+  const ref = useRef(isDropdown);
 
-  static defaultProps = {
-    renderPrefix: undefined,
-    renderSuffix: undefined,
-    className: "",
-    isOutline: false,
-    color: undefined,
-    skin: "pale",
-    disabled: false,
-    ariaDisabled: false,
-    onClick: () => {},
-    onInteraction: () => {},
-    setRef: () => {},
-    tabIndex: undefined,
-    isDropdown: false,
-    isSelected: false,
-    value: "",
-    role: "",
-  };
-
-  constructor(props) {
-    super(props);
-
-    if (props.isDropdown) this.ref = React.createRef();
-  }
-
-  componentDidMount() {
-    const { setRef, isDropdown } = this.props;
-    if (isDropdown) setRef(this.ref);
-  }
-
-  handleEvent = (event) => {
-    const { onClick, onInteraction } = this.props;
-
-    if ((event.keycode || event.which) === 32) {
+  const handleEvent = (event) => {
+    if ((event.key || event.which) === 32) {
       event.preventDefault();
     }
     if (!isUndefined(onClick)) {
@@ -81,71 +32,114 @@ export default class AbstractTag extends React.Component {
     }
 
     if (!isUndefined(onInteraction)) {
-      onInteraction({ event, ref: this.ref });
+      onInteraction({ event, ref });
     }
   };
 
-  render() {
-    const {
-      text,
-      disabled,
-      ariaDisabled,
-      isOutline,
-      skin,
-      color,
-      renderPrefix,
-      renderSuffix,
-      className,
-      tabIndex,
-      isDropdown,
-      value,
-      isSelected,
-      role,
-    } = this.props;
+  useEffect(() => {
+    if (isDropdown) {
+      setRef(ref);
+    }
+  }, [isDropdown]);
 
-    if (isDropdown)
-      return (
-        <button
-          type="button"
-          role={role}
-          aria-selected={isSelected}
-          className={
-            `lab-tag ${className}` +
-            `${disabled || ariaDisabled ? ` lab-tag--disabled` : ""}` +
-            `${isOutline ? ` lab-tag--outline` : ""}` +
-            `${color ? ` lab-tag--${color}-${skin}` : ` lab-tag--${skin}`}`
-          }
-          onClick={!(ariaDisabled || disabled) ? this.handleEvent : () => {}}
-          onKeyDown={this.handleEvent}
-          tabIndex={tabIndex}
-          ref={this.ref}
-          disabled={disabled || ariaDisabled}
-          id={value}
-          value={value}
-        >
-          {renderPrefix}
-          {text}
-          {renderSuffix}
-        </button>
-      );
-
+  if (isDropdown)
     return (
-      <span
+      <button
+        type="button"
+        role={role}
+        aria-selected={isSelected}
         className={
           `lab-tag ${className}` +
           `${disabled || ariaDisabled ? ` lab-tag--disabled` : ""}` +
           `${isOutline ? ` lab-tag--outline` : ""}` +
           `${color ? ` lab-tag--${color}-${skin}` : ` lab-tag--${skin}`}`
         }
-        onClick={!(ariaDisabled || disabled) ? this.handleEvent : () => {}}
-        onKeyPress={this.handleEvent}
-        role="button"
+        onClick={!(ariaDisabled || disabled) ? handleEvent : () => {}}
+        onKeyDown={handleEvent}
         tabIndex={tabIndex}
+        ref={ref}
+        disabled={disabled || ariaDisabled}
+        id={value}
+        value={value}
       >
         {renderPrefix}
         {text}
         {renderSuffix}
-      </span>
+      </button>
     );
-  }
+
+  return (
+    <span
+      className={
+        `lab-tag ${className}` +
+        `${disabled || ariaDisabled ? ` lab-tag--disabled` : ""}` +
+        `${isOutline ? ` lab-tag--outline` : ""}` +
+        `${color ? ` lab-tag--${color}-${skin}` : ` lab-tag--${skin}`}`
+      }
+      onClick={!(ariaDisabled || disabled) ? handleEvent : () => {}}
+      onKeyDown={handleEvent}
+      role="button"
+      tabIndex={tabIndex}
+    >
+      {renderPrefix}
+      {text}
+      {renderSuffix}
+    </span>
+  );
 }
+
+AbstractTag.propTypes = {
+  /** Tag's left element (optional and customizable) */
+  renderPrefix: PropTypes.object,
+  /** Components to be rendered at the end of the Tag. */
+  renderSuffix: PropTypes.object,
+  /** Defines a class name to create a custom style for the component. */
+  className: PropTypes.string,
+  /** This is the Tag's text. */
+  text: PropTypes.string.isRequired,
+  /** Sets Tag's color. */
+  color: PropTypes.string,
+  /** Skin of the rendered Tag. */
+  skin: PropTypes.string,
+  /** Sets an outline style. */
+  isOutline: PropTypes.bool,
+  /** Disables the Tag. Will be read by screen readers. When true, will override `disabled`. */
+  ariaDisabled: PropTypes.bool,
+  /** Disables the Tag. Won't be read by screen readers. */
+  disabled: PropTypes.bool,
+  /** Action to be executed when the Tag is clicked. */
+  onClick: PropTypes.func,
+  /** tabIndex is used to define the navigation order for focusable elements. If not undefined, it is passed to the tag component. */
+  tabIndex: PropTypes.string,
+  /** This prop is a boolean to verify if the tag is a child of the TagDropdown component */
+  isDropdown: PropTypes.bool,
+  /** This function is used to handle click or keydown interactions */
+  onInteraction: PropTypes.func,
+  /** This is the TagDropdownItem value. It is used TagDropdown options */
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /**  This prop is used to verify if the option is selected */
+  isSelected: PropTypes.bool,
+  /**   This is the TagDropdownItem or TagDropdownTrigger role. It is used to let disabled people know the type of the element. */
+  role: PropTypes.string,
+  /** This function is used on AbstractTag to set the current Ref */
+  setRef: PropTypes.func,
+};
+
+AbstractTag.defaultProps = {
+  renderPrefix: undefined,
+  renderSuffix: undefined,
+  className: "",
+  isOutline: false,
+  color: undefined,
+  skin: "pale",
+  disabled: false,
+  ariaDisabled: false,
+  onClick: () => {},
+  onInteraction: () => {},
+  setRef: () => {},
+  tabIndex: undefined,
+  isDropdown: false,
+  isSelected: false,
+  value: "",
+  role: "",
+};
--- packages/confetti-ds/src/Tags/RemovableTag.js
+++ packages/confetti-ds/src/Tags/RemovableTag.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
 import { isEmpty } from "lodash";
 
@@ -6,49 +6,18 @@ import AbstractTag from "./AbstractTag";
 import Icon from "../Icon";
 import { ICON_TYPES, TAG_COLORS } from "../constants";
 
-export default class RemovableTag extends React.Component {
-  static propTypes = {
-    /** This is the Tag's text. */
-    text: PropTypes.string.isRequired,
-    /** Source of the thumbnail to be rendered. Won't render a thumbnail if not passed to the component. Can't have both 'icon' and 'thumbSrc' at the same time. */
-    thumbSrc: PropTypes.string,
-    /** Type of the icon to be rendered. Won't render an icon if not passed to the component. Can't have both 'icon' and 'thumbSrc' at the same time. */
-    icon: PropTypes.oneOf(ICON_TYPES),
-    /** Sets Tag's color. */
-    color: PropTypes.oneOf(TAG_COLORS),
-    /** Skin of the the rendered Tag. */
-    skin: PropTypes.oneOf(["pale", "vivid"]),
-    /** Sets an outline style. */
-    isOutline: PropTypes.bool,
-    /** Disables the Tag. Will be read by screen readers. When true, will override `disabled`. */
-    ariaDisabled: PropTypes.bool,
-    /** Disables the Tag. Won't be read by screen readers. */
-    disabled: PropTypes.bool,
-    /** Callback to be executed when the Tag is clicked. */
-    onClick: PropTypes.func,
-  };
-
-  static defaultProps = {
-    thumbSrc: "",
-    icon: undefined,
-    color: undefined,
-    skin: "pale",
-    isOutline: false,
-    disabled: false,
-    ariaDisabled: false,
-    onClick: () => {},
-  };
-
-  constructor(props) {
-    super(props);
-    this.checkThumbAndIcon();
-  }
-
-  componentDidUpdate() {
-    this.checkThumbAndIcon();
-  }
-
-  removeIcon = () => (
+export default function RemovableTag({
+  text,
+  thumbSrc,
+  icon,
+  color,
+  skin,
+  isOutline,
+  disabled,
+  ariaDisabled,
+  onClick,
+}) {
+  const removeIcon = () => (
     <span className="lab-tag__remove-icon-wrapper">
       <Icon
         type="remove"
@@ -59,16 +28,13 @@ export default class RemovableTag extends React.Component {
     </span>
   );
 
-  thumb = () => {
-    const { thumbSrc } = this.props;
-    return thumbSrc ? (
+  const thumb = () =>
+    thumbSrc ? (
       <img className="lab-tag__thumb" src={thumbSrc} alt="" />
     ) : undefined;
-  };
 
-  icon = () => {
-    const { icon } = this.props;
-    return icon ? (
+  const handleIcon = () =>
+    icon ? (
       <Icon
         type={icon}
         color="black-75"
@@ -76,47 +42,68 @@ export default class RemovableTag extends React.Component {
         className="lab-tag--left-icon"
       />
     ) : undefined;
-  };
 
-  checkThumbAndIcon() {
+  const checkThumbAndIcon = () => {
     const errorMessage =
       "`RemovableTag` can't be initialized with both `thumb` and `icon` props.";
-    const { thumbSrc, icon } = this.props;
     if (!isEmpty(thumbSrc) && !isEmpty(icon)) {
       throw Error(errorMessage);
     }
-  }
+  };
 
-  render() {
-    const {
-      text,
-      thumbSrc,
-      icon,
-      color,
-      skin,
-      isOutline,
-      disabled,
-      ariaDisabled,
-      onClick,
-    } = this.props;
-    return (
-      <AbstractTag
-        className={`lab-tag--removable${`${
-          icon ? ` lab-tag--has-left-icon` : ""
-        }${thumbSrc ? ` lab-tag--has-thumb` : ""}`}`}
-        text={text}
-        thumbSrc={thumbSrc}
-        icon={icon}
-        color={color}
-        skin={skin}
-        isOutline={isOutline}
-        disabled={!ariaDisabled && disabled}
-        ariaDisabled={ariaDisabled}
-        onClick={onClick}
-        renderPrefix={this.thumb() || this.icon()}
-        renderSuffix={this.removeIcon()}
-        tabIndex="0"
-      />
-    );
-  }
+  useMemo(() => {
+    checkThumbAndIcon();
+  }, [icon, thumbSrc]);
+
+  return (
+    <AbstractTag
+      className={`lab-tag--removable${`${
+        icon ? ` lab-tag--has-left-icon` : ""
+      }${thumbSrc ? ` lab-tag--has-thumb` : ""}`}`}
+      text={text}
+      thumbSrc={thumbSrc}
+      icon={icon}
+      color={color}
+      skin={skin}
+      isOutline={isOutline}
+      disabled={!ariaDisabled && disabled}
+      ariaDisabled={ariaDisabled}
+      onClick={onClick}
+      renderPrefix={thumb() || handleIcon()}
+      renderSuffix={removeIcon()}
+      tabIndex="0"
+    />
+  );
 }
+
+RemovableTag.propTypes = {
+  /** This is the Tag's text. */
+  text: PropTypes.string.isRequired,
+  /** Source of the thumbnail to be rendered. Won't render a thumbnail if not passed to the component. Can't have both 'icon' and 'thumbSrc' at the same time. */
+  thumbSrc: PropTypes.string,
+  /** Type of the icon to be rendered. Won't render an icon if not passed to the component. Can't have both 'icon' and 'thumbSrc' at the same time. */
+  icon: PropTypes.oneOf(ICON_TYPES),
+  /** Sets Tag's color. */
+  color: PropTypes.oneOf(TAG_COLORS),
+  /** Skin of the rendered Tag. */
+  skin: PropTypes.oneOf(["pale", "vivid"]),
+  /** Sets an outline style. */
+  isOutline: PropTypes.bool,
+  /** Disables the Tag. Will be read by screen readers. When true, will override `disabled`. */
+  ariaDisabled: PropTypes.bool,
+  /** Disables the Tag. Won't be read by screen readers. */
+  disabled: PropTypes.bool,
+  /** Callback to be executed when the Tag is clicked. */
+  onClick: PropTypes.func,
+};
+
+RemovableTag.defaultProps = {
+  thumbSrc: "",
+  icon: undefined,
+  color: undefined,
+  skin: "pale",
+  isOutline: false,
+  disabled: false,
+  ariaDisabled: false,
+  onClick: () => {},
+};
