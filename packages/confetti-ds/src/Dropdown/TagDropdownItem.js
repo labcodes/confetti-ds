894ebad3e72b0157f1b4ac904bb97b9dd27c15a8
Juliana Barros Lima <12106738+tiidadavena@users.noreply.github.com>
2022-09-12 11:28:25 -0300

    [CSYS-366] Migrating DropdownSectionTitle, TagDropdownItem and TagDropdown to Hooks (#50)
    * refactor: migrating DropdownSectionTitle to functional component
    
    * refactor: migrating TagDropdown to functional component
    
    * refactor: removing the methods and refs from the Class Components statements
    
    * refactor: adding the stateful Hooks to control better the component's lifecycle
    
    * tests: adding an async method to handle better from the promises from the Hooks
    
    * fix: removing comments

--- packages/confetti-ds/src/Dropdown/TagDropdownItem.js
+++ packages/confetti-ds/src/Dropdown/TagDropdownItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
 import { isEmpty } from "lodash";
 
@@ -6,69 +6,28 @@ import Icon from "../Icon";
 import { ICON_TYPES, TAG_COLORS } from "../constants";
 import AbstractTag from "../Tags/AbstractTag";
 
-export default class TagDropdownItem extends React.Component {
-  static propTypes = {
-    /** This is the Tag's text. */
-    text: PropTypes.string.isRequired,
-    /** Disables the Tag. Won't be read by screen readers. */
-    disabled: PropTypes.bool,
-    /** Action to be executed when the Tag is clicked. */
-    onClick: PropTypes.func,
-    /** Source of the thumb to be rendered. Won't render a thumb if not passed to the component. Can't have both 'icon' and 'thumbSrc' at the same time. */
-    thumbSrc: PropTypes.string,
-    /** Type of the icon to be rendered. Won't render an icon if not passed to the component. Can't have both 'icon' and 'thumbSrc' at the same time. */
-    icon: PropTypes.oneOf(ICON_TYPES),
-    /** Sets Tag's color. */
-    color: PropTypes.oneOf(TAG_COLORS),
-    /** Skin of the the rendered Tag. */
-    skin: PropTypes.oneOf(["pale", "vivid"]),
-    /** Sets an outline style. */
-    isOutline: PropTypes.bool,
-    /** Option tab index */
-    tabIndex: PropTypes.string,
-    /** This function is used on AbstractTag's componenentDidMount to set the current Ref */
-    setRef: PropTypes.func,
-    /** This function is used to handle click or keydown interactions */
-    onInteraction: PropTypes.func,
-    /** This is the option's value */
-    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-    /** This prop is a boolean to verify if the option is current selected  */
-    isSelected: PropTypes.bool,
-  };
-
-  static defaultProps = {
-    thumbSrc: "",
-    icon: undefined,
-    isOutline: false,
-    skin: "pale",
-    color: undefined,
-    disabled: false,
-    onClick: () => {},
-    setRef: () => {},
-    onInteraction: () => {},
-    tabIndex: "0",
-    isSelected: false,
-  };
-
-  constructor(props) {
-    super(props);
-    this.checkThumbAndIcon();
-  }
-
-  componentDidUpdate() {
-    this.checkThumbAndIcon();
-  }
-
-  thumb = () => {
-    const { thumbSrc } = this.props;
-    return thumbSrc ? (
+export default function TagDropdownItem({
+  text,
+  thumbSrc,
+  icon,
+  color,
+  skin,
+  isOutline,
+  onClick,
+  disabled,
+  tabIndex,
+  onInteraction,
+  setRef,
+  value,
+  isSelected,
+}) {
+  const renderThumb = () =>
+    thumbSrc ? (
       <img className="lab-tag__thumb" src={thumbSrc} alt="" />
     ) : undefined;
-  };
 
-  icon = () => {
-    const { icon } = this.props;
-    return icon ? (
+  const renderIcon = () =>
+    icon ? (
       <Icon
         type={icon}
         color="black-75"
@@ -76,53 +35,83 @@ export default class TagDropdownItem extends React.Component {
         className="lab-tag--left-icon"
       />
     ) : undefined;
-  };
 
-  checkThumbAndIcon() {
+  // super constructor
+  const checkThumbAndIcon = useCallback(() => {
     const errorMessage =
       "`TagDropdownItem` can't be initialized with both `thumb` and `icon` props.";
-    const { thumbSrc, icon } = this.props;
     if (!isEmpty(thumbSrc) && !isEmpty(icon)) {
       throw new Error(errorMessage);
     }
-  }
+  }, [thumbSrc, icon]);
 
-  render() {
-    const {
-      text,
-      thumbSrc,
-      icon,
-      color,
-      skin,
-      isOutline,
-      onClick,
-      disabled,
-      tabIndex,
-      onInteraction,
-      setRef,
-      value,
-      isSelected,
-    } = this.props;
-    return (
-      <AbstractTag
-        text={text}
-        className={`lab-tag--togglable${icon ? ` lab-tag--has-left-icon` : ""}${
-          thumbSrc ? ` lab-tag--has-thumb` : ""
-        }`}
-        isOutline={isOutline}
-        skin={skin}
-        color={color}
-        renderPrefix={this.icon() || this.thumb()}
-        onClick={onClick}
-        disabled={disabled}
-        tabIndex={tabIndex}
-        onInteraction={onInteraction}
-        isDropdown
-        setRef={setRef}
-        value={value}
-        isSelected={isSelected}
-        role="option"
-      />
-    );
-  }
+  // componentDidUpdate
+  useEffect(() => {
+    checkThumbAndIcon();
+  }, [thumbSrc, icon]);
+
+  return (
+    <AbstractTag
+      text={text}
+      className={`lab-tag--togglable${icon ? ` lab-tag--has-left-icon` : ""}${
+        thumbSrc ? ` lab-tag--has-thumb` : ""
+      }`}
+      isOutline={isOutline}
+      skin={skin}
+      color={color}
+      renderPrefix={renderThumb() || renderIcon()}
+      onClick={onClick}
+      disabled={disabled}
+      tabIndex={tabIndex}
+      onInteraction={onInteraction}
+      isDropdown
+      setRef={setRef}
+      value={value}
+      isSelected={isSelected}
+      role="option"
+    />
+  );
 }
+
+TagDropdownItem.propTypes = {
+  /** This is the Tag's text. */
+  text: PropTypes.string.isRequired,
+  /** Disables the Tag. Won't be read by screen readers. */
+  disabled: PropTypes.bool,
+  /** Action to be executed when the Tag is clicked. */
+  onClick: PropTypes.func,
+  /** Source of the thumb to be rendered. Won't render a thumb if not passed to the component. Can't have both 'icon' and 'thumbSrc' at the same time. */
+  thumbSrc: PropTypes.string,
+  /** Type of the icon to be rendered. Won't render an icon if not passed to the component. Can't have both 'icon' and 'thumbSrc' at the same time. */
+  icon: PropTypes.oneOf(ICON_TYPES),
+  /** Sets Tag's color. */
+  color: PropTypes.oneOf(TAG_COLORS),
+  /** Skin of the rendered Tag. */
+  skin: PropTypes.oneOf(["pale", "vivid"]),
+  /** Sets an outline style. */
+  isOutline: PropTypes.bool,
+  /** Option tab index */
+  tabIndex: PropTypes.string,
+  /** This function is used on AbstractTag's componenentDidMount to set the current Ref */
+  setRef: PropTypes.func,
+  /** This function is used to handle click or keydown interactions */
+  onInteraction: PropTypes.func,
+  /** This is the option's value */
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  /** This prop is a boolean to verify if the option is current selected  */
+  isSelected: PropTypes.bool,
+};
+
+TagDropdownItem.defaultProps = {
+  thumbSrc: "",
+  icon: undefined,
+  isOutline: false,
+  skin: "pale",
+  color: undefined,
+  disabled: false,
+  onClick: () => {},
+  setRef: () => {},
+  onInteraction: () => {},
+  tabIndex: "0",
+  isSelected: false,
+};
