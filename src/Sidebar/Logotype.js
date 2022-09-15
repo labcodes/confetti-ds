e4773d75e1547f423f99809b1a626ed69c01989d
daniel carvalho <42525687+ddevdan@users.noreply.github.com>
2022-09-15 14:15:26 -0300

    [CSYS-317] Migrate Sidebar to function component (#34)
    * Change Sidebar.Body from class to fn component
    
    * Change Sidebar.CollapseButton from class to fn component
    
    * Change Sidebar.Footer from class to fn component
    
    * Change Sidebar.FooterButton from class to fn component
    
    * Change Sidebar.Header from class to fn component
    
    * Change Sidebar.LogoType from class to fn component
    
    * Change Sidebar.NarrowSidebar from class to fn component
    
    * Change Sidebar.UserAvatar from class to fn component

--- src/Sidebar/Logotype.js
+++ src/Sidebar/Logotype.js
@@ -1,20 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default class Logotype extends React.Component {
-  static propTypes = {
-    /** Source of the logo to be rendered. */
-    logoSrc: PropTypes.string.isRequired,
-    /** Text that will specify the HTML alt attribute of an <img> element. */
-    altText: PropTypes.string.isRequired,
-  };
-
-  render() {
-    const { logoSrc, altText } = this.props;
-    return (
-      <div className="lab-narrow-sidebar__logo">
-        <img src={logoSrc} alt={altText} />
-      </div>
-    );
-  }
+/**
+ *
+ * @param logoSrc Source of the logo to be rendered.
+ * @param altText Text that will specify the HTML alt attribute of an <img> element.
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function Logotype({ logoSrc, altText }) {
+  return (
+    <div className="lab-narrow-sidebar__logo">
+      <img src={logoSrc} alt={altText} />
+    </div>
+  );
 }
+
+Logotype.propTypes = {
+  /** Source of the logo to be rendered. */
+  logoSrc: PropTypes.string.isRequired,
+  /** Text that will specify the HTML alt attribute of an <img> element. */
+  altText: PropTypes.string.isRequired,
+};
