c777ffbdfdf8cc9baafa29082e57799693f9e791
daniel carvalho <42525687+ddevdan@users.noreply.github.com>
2022-09-15 14:49:12 -0300

    [CSYS-314] Migrate Dialog to function component (#36)
    * Change DialogWrapper.js from class to fn component
    
    * Add DialogWrapper tests
    
    * Change MessageDialog.js from class to fn component
    
    * Change StandardDialog.js from class to fn component
    
    * Add snapshots
    
    * add assertion to tests
    
    * add disable eslint no-alert rule at MessageDialog.test.js, StandardDialog.test.js

--- packages/confetti-ds/src/Dialog/DialogWrapper.js
+++ packages/confetti-ds/src/Dialog/DialogWrapper.js
@@ -2,80 +2,60 @@ import React from "react";
 import PropTypes from "prop-types";
 import FocusTrap from "focus-trap-react";
 
-export default class DialogWrapper extends React.Component {
-  static propTypes = {
-    handleClose: PropTypes.func.isRequired,
-    isOpen: PropTypes.bool.isRequired,
-    isModal: PropTypes.bool.isRequired,
-    children: PropTypes.node.isRequired,
-    isMessageDialog: PropTypes.bool,
-    isLarge: PropTypes.bool,
-  };
+/**
+ *
+ * @param isOpen Toggles overflow based on is open state. It changes the state shouldToggleOverflow
+ * @param isModal Toggles overflow based on is a modal state. It changes the state shouldToggleOverflow
+ * @param handleClose This function prop is called on a close button or outside click event
+ * @param isLarge Toggles .lab-dialog--large classname to increase Dialog width
+ * @param isMessageDialog Toggles .lab-dialog--large classname to increase Dialog width
+ * @param children Components that will be rendered in the DialogWrapper (MessageDialog, StandardDialog)
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function DialogWrapper({
+  isOpen,
+  isModal,
+  handleClose,
+  isLarge,
+  isMessageDialog,
+  children,
+}) {
+  const deviceIsMobile = window.outerWidth <= 768;
+  const [shouldToggleOverflow, setShouldToggleOverflow] = React.useState(
+    isOpen && (isModal || deviceIsMobile)
+  );
 
-  static defaultProps = {
-    isMessageDialog: undefined,
-    isLarge: undefined,
-  };
-
-  constructor(props) {
-    super(props);
-    const { isOpen, isModal } = props;
+  const handleResize = () => {
     const deviceIsMobile = window.outerWidth <= 768;
-    this.state = {
-      shouldToggleOverflow: isOpen && (isModal || deviceIsMobile),
-    };
-  }
+    setShouldToggleOverflow(isOpen && (isModal || deviceIsMobile));
+  };
 
-  componentDidMount() {
-    window.addEventListener("resize", this.handleResize);
-    this.handleOverflow();
-  }
-
-  componentDidUpdate() {
-    this.handleOverflow();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.handleResize, false);
-    document.body.style.overflow = "unset";
-  }
-
-  handleOverflow = () => {
-    const { shouldToggleOverflow } = this.state;
+  const handleOverflow = () => {
     document.body.style.overflow = shouldToggleOverflow ? "hidden" : "unset";
   };
 
-  handleResize = () => {
-    const { isOpen, isModal } = this.props;
-    const deviceIsMobile = window.outerWidth <= 768;
-    this.setState({
-      shouldToggleOverflow: isOpen && (isModal || deviceIsMobile),
-    });
-  };
+  React.useEffect(() => {
+    window.addEventListener("resize", handleResize);
 
-  render() {
-    const { handleClose, isLarge, isMessageDialog, children } = this.props;
-    const { shouldToggleOverflow } = this.state;
-    return shouldToggleOverflow ? (
-      <React.Fragment>
-        <div
-          className="lab-dialog-overlay"
-          onClick={handleClose}
-          role="presentation"
-        />
-        <FocusTrap focusTrapOptions={{ allowOutsideClick: true }}>
-          <div
-            className={`lab-dialog${
-              isMessageDialog ? " lab-dialog--message" : " lab-dialog--standard"
-            }
-            ${isLarge ? ` lab-dialog--large` : ""}`}
-          >
-            {children}
-          </div>
-        </FocusTrap>
-      </React.Fragment>
-    ) : (
-      <React.Fragment>
+    return () => {
+      window.removeEventListener("resize", handleResize, false);
+      document.body.style.overflow = "unset";
+    };
+  }, []);
+
+  React.useEffect(() => {
+    handleOverflow();
+  }, [shouldToggleOverflow, isOpen, isModal, deviceIsMobile]);
+
+  return shouldToggleOverflow ? (
+    <React.Fragment>
+      <div
+        className="lab-dialog-overlay"
+        onClick={handleClose}
+        role="presentation"
+      />
+      <FocusTrap focusTrapOptions={{ allowOutsideClick: true }}>
         <div
           className={`lab-dialog${
             isMessageDialog ? " lab-dialog--message" : " lab-dialog--standard"
@@ -84,7 +64,38 @@ export default class DialogWrapper extends React.Component {
         >
           {children}
         </div>
-      </React.Fragment>
-    );
-  }
+      </FocusTrap>
+    </React.Fragment>
+  ) : (
+    <React.Fragment>
+      <div
+        className={`lab-dialog${
+          isMessageDialog ? " lab-dialog--message" : " lab-dialog--standard"
+        }
+            ${isLarge ? ` lab-dialog--large` : ""}`}
+      >
+        {children}
+      </div>
+    </React.Fragment>
+  );
 }
+
+DialogWrapper.propTypes = {
+  /** This function prop is called on a close button or outside click event */
+  handleClose: PropTypes.func.isRequired,
+  /** Toggles overflow based on is open state. It changes the state shouldToggleOverflow . */
+  isOpen: PropTypes.bool.isRequired,
+  /** Toggles overflow based on is a modal state. It changes the state shouldToggleOverflow . */
+  isModal: PropTypes.bool.isRequired,
+  /** Components that will be rendered in the DialogWrapper (MessageDialog, StandardDialog) */
+  children: PropTypes.node.isRequired,
+  /** Toggles .lab-dialog--message classname */
+  isMessageDialog: PropTypes.bool,
+  /** Toggles .lab-dialog--large classname to increase Dialog width */
+  isLarge: PropTypes.bool,
+};
+
+DialogWrapper.defaultProps = {
+  isMessageDialog: undefined,
+  isLarge: undefined,
+};
