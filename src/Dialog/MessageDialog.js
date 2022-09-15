b10f6ca341893596cc81e79f67114535769355df
daniel carvalho <42525687+ddevdan@users.noreply.github.com>
2022-09-15 14:49:12 -0300

    [CSYS-314] Migrate Dialog to function component (#36)
    * Change DialogWrapper.js from class to fn component
    
    * Add DialogWrapper tests
    
    * Change MessageDialog.js from class to fn component
    
    * Change StandardDialog.js from class to fn component
    
    * Add snapshots
    
    * add assertion to tests
    
    * add disable eslint no-alert rule at MessageDialog.test.js, StandardDialog.test.js

--- src/Dialog/MessageDialog.js
+++ src/Dialog/MessageDialog.js
@@ -6,149 +6,136 @@ import DialogWrapper from "./DialogWrapper";
 import { Button, OutlineButton } from "../Button";
 import { ICON_TYPES } from "../constants";
 
-export default class MessageDialog extends React.Component {
-  static propTypes = {
-    icon: PropTypes.oneOf(ICON_TYPES).isRequired,
-    title: PropTypes.string.isRequired,
-    content: PropTypes.string.isRequired,
-    buttonProps: PropTypes.exact({
-      text: PropTypes.string.isRequired,
-      onClick: PropTypes.func.isRequired,
-    }).isRequired,
-    outlineButtonProps: PropTypes.exact({
-      text: PropTypes.string.isRequired,
-      onClick: PropTypes.func.isRequired,
-    }),
-    isLarge: PropTypes.bool,
-    handleClose: PropTypes.func,
-    isOpen: PropTypes.bool,
-    isModal: PropTypes.bool,
-  };
+export default function MessageDialog({
+  icon,
+  title,
+  content,
+  buttonProps,
+  outlineButtonProps,
+  isLarge,
+  isOpen,
+  handleClose,
+  isModal,
+}) {
+  const [swipeStartYCoordinate, setSwipeStartYCoordinate] =
+    React.useState(undefined);
 
-  static defaultProps = {
-    outlineButtonProps: undefined,
-    isLarge: false,
-    handleClose: () => {},
-    isOpen: false,
-    isModal: false,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      swipeStartYCoordinate: undefined,
-    };
-  }
-
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyDown, false);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyDown, false);
-  }
-
-  handleKeyDown = (event) => {
-    const { handleClose } = this.props;
+  const handleKeyDown = (event) => {
     if (event.keyCode === 27) {
       handleClose();
     }
   };
 
-  handleTouchStart = (event) => {
-    this.setState({ swipeStartYCoordinate: event.changedTouches[0].screenY });
+  const handleTouchStart = (event) => {
+    setSwipeStartYCoordinate(event.changedTouches[0].screenY);
   };
 
-  handleTouchEnd = (event) => {
-    const { handleClose } = this.props;
-    const { swipeStartYCoordinate } = this.state;
-
+  const handleTouchEnd = (event) => {
     if (event.changedTouches[0].screenY - swipeStartYCoordinate >= 75) {
-      this.setState({ swipeStartYCoordinate: undefined }, () => handleClose());
+      setSwipeStartYCoordinate(undefined);
+      handleClose();
     } else {
-      this.setState({ swipeStartYCoordinate: undefined });
+      setSwipeStartYCoordinate(undefined);
     }
   };
 
-  render() {
-    const {
-      icon,
-      title,
-      content,
-      buttonProps,
-      outlineButtonProps,
-      isLarge,
-      isOpen,
-      handleClose,
-      isModal,
-    } = this.props;
+  React.useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown, false);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown, false);
+    };
+  }, []);
 
-    if (!isOpen) return null;
-    return (
-      <DialogWrapper
-        handleClose={handleClose}
-        isOpen={isOpen}
-        isModal={isModal}
-        isLarge={isLarge}
-        isMessageDialog
+  if (!isOpen) return null;
+  return (
+    <DialogWrapper
+      handleClose={handleClose}
+      isOpen={isOpen}
+      isModal={isModal}
+      isLarge={isLarge}
+      isMessageDialog
+    >
+      <div
+        className="lab-dialog__content lab-dialog__content--message"
+        role="dialog"
+        aria-modal="true"
       >
-        <div
-          className="lab-dialog__content lab-dialog__content--message"
-          role="dialog"
-          aria-modal="true"
+        <button
+          type="button"
+          className="lab-dialog__mobile-close-button lab-dialog__mobile-close-button--message"
+          onClick={handleClose}
+          onTouchStart={handleTouchStart}
+          onTouchEnd={handleTouchEnd}
         >
+          <Icon type="collapse-open" />
+        </button>
+        <div className="lab-dialog__header lab-dialog__header--message">
           <button
+            className="lab-dialog__close-button"
             type="button"
-            className="lab-dialog__mobile-close-button lab-dialog__mobile-close-button--message"
+            {...(isModal ? { tabIndex: "2" } : undefined)}
             onClick={handleClose}
-            onTouchStart={this.handleTouchStart}
-            onTouchEnd={this.handleTouchEnd}
           >
-            <Icon type="collapse-open" />
+            <Icon type="remove" className="lab-dialog__close-button-icon" />
           </button>
-          <div className="lab-dialog__header lab-dialog__header--message">
-            <button
-              className="lab-dialog__close-button"
-              type="button"
-              {...(isModal ? { tabIndex: "2" } : undefined)}
-              onClick={handleClose}
-            >
-              <Icon type="remove" className="lab-dialog__close-button-icon" />
-            </button>
-          </div>
-
-          <div className="lab-dialog__icon-wrapper">
-            <Icon type={icon} className="lab-dialog__icon" />
-          </div>
-
-          <div className="lab-dialog__title lab-dialog__title--message">
-            {title}
-          </div>
-
-          <div className="lab-dialog__body lab-dialog__body--message">
-            {content}
-          </div>
-
-          <div className="lab-dialog__footer lab-dialog__footer--message">
-            {outlineButtonProps ? (
-              <OutlineButton
-                size="normal"
-                text={outlineButtonProps.text}
-                onClick={outlineButtonProps.onClick}
-                {...(isModal ? { tabIndex: "3" } : undefined)}
-              />
-            ) : undefined}
-            <Button
-              size="normal"
-              {...(outlineButtonProps ? undefined : { fullWidth: true })}
-              text={buttonProps.text}
-              onClick={buttonProps.onClick}
-              {...(isModal ? { tabIndex: "1" } : undefined)}
-            />
-          </div>
         </div>
-      </DialogWrapper>
-    );
-  }
+
+        <div className="lab-dialog__icon-wrapper">
+          <Icon type={icon} className="lab-dialog__icon" />
+        </div>
+
+        <div className="lab-dialog__title lab-dialog__title--message">
+          {title}
+        </div>
+
+        <div className="lab-dialog__body lab-dialog__body--message">
+          {content}
+        </div>
+
+        <div className="lab-dialog__footer lab-dialog__footer--message">
+          {outlineButtonProps ? (
+            <OutlineButton
+              size="normal"
+              text={outlineButtonProps.text}
+              onClick={outlineButtonProps.onClick}
+              {...(isModal ? { tabIndex: "3" } : undefined)}
+            />
+          ) : undefined}
+          <Button
+            size="normal"
+            {...(outlineButtonProps ? undefined : { fullWidth: true })}
+            text={buttonProps.text}
+            onClick={buttonProps.onClick}
+            {...(isModal ? { tabIndex: "1" } : undefined)}
+          />
+        </div>
+      </div>
+    </DialogWrapper>
+  );
 }
+
+MessageDialog.propTypes = {
+  icon: PropTypes.oneOf(ICON_TYPES).isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  buttonProps: PropTypes.exact({
+    text: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+  }).isRequired,
+  outlineButtonProps: PropTypes.exact({
+    text: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+  }),
+  isLarge: PropTypes.bool,
+  handleClose: PropTypes.func,
+  isOpen: PropTypes.bool,
+  isModal: PropTypes.bool,
+};
+
+MessageDialog.defaultProps = {
+  outlineButtonProps: undefined,
+  isLarge: false,
+  handleClose: () => {},
+  isOpen: false,
+  isModal: false,
+};
