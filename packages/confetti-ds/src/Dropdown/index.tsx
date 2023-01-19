ce6331b39bab1b55547ab3ddfb025b4ac76bcea9
Luciano Ratamero <luciano@ratamero.com>
2023-01-19 19:06:47 -0300

    feat: fix tsc errors

--- packages/confetti-ds/src/Dropdown/index.tsx
+++ packages/confetti-ds/src/Dropdown/index.tsx
@@ -2,7 +2,7 @@ import TagDropdown from "./TagDropdown";
 import TagDropdownItem from "./TagDropdownItem";
 import SectionTitle from "./DropdownSectionTitle";
 
-TagDropdown.TagItem = TagDropdownItem;
-TagDropdown.SectionTitle = SectionTitle;
+TagDropdown["TagItem"] = TagDropdownItem;
+TagDropdown["SectionTitle"] = SectionTitle;
 
 export { TagDropdown };
--- packages/confetti-ds/src/Sidebar/index.tsx
+++ packages/confetti-ds/src/Sidebar/index.tsx
@@ -8,13 +8,13 @@ import Logotype from "./Logotype";
 import CollapseButton from "./CollapseButton";
 import Item from "./Item";
 
-NarrowSidebar.Header = Header;
-NarrowSidebar.Body = Body;
-NarrowSidebar.Footer = Footer;
-NarrowSidebar.FooterButton = FooterButton;
-NarrowSidebar.UserAvatar = UserAvatar;
-NarrowSidebar.Logotype = Logotype;
-NarrowSidebar.CollapseButton = CollapseButton;
-NarrowSidebar.Item = Item;
+NarrowSidebar["Header"] = Header;
+NarrowSidebar["Body"] = Body;
+NarrowSidebar["Footer"] = Footer;
+NarrowSidebar["FooterButton"] = FooterButton;
+NarrowSidebar["UserAvatar"] = UserAvatar;
+NarrowSidebar["Logotype"] = Logotype;
+NarrowSidebar["CollapseButton"] = CollapseButton;
+NarrowSidebar["Item"] = Item;
 
 export default NarrowSidebar;
