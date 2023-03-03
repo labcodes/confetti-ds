2ee2ad4f7ed900b747dd3e178c0ce4776b8be5a0
Juliana Barros Lima <judiembl@gmail.com>
2023-03-03 17:39:55 -0300

    chore: implementing the poc

--- packages/confetti-ds/src/Icon.tsx
+++ packages/confetti-ds/src/Icon.tsx
@@ -1,26 +1,39 @@
 import React from "react";
 
 import { IconTypes, IconColorTypes } from "./constants";
-
+import {
+  ArrowLeft20Regular,
+  Delete20Regular,
+  ArrowMinimize20Regular,
+  StackStar20Regular,
+} from "@fluentui/react-icons";
 export interface IconProps {
   /** Type of the Icon to be rendered. */
   type: IconTypes;
   /** Color of the rendered Icon. */
   color?: IconColorTypes;
+  // TODO: vai de base tb
   /** Size of the rendered Icon, petit = 16px, small = 20px. If omitted will render at 24px. */
   size?: "small" | "petit";
   /** Add a class name to make custom changes */
   className?: string;
 }
 export default function Icon({ type, color, size, className }: IconProps) {
+  let IconComponent;
+
+  if (type === "arrow-left") IconComponent = ArrowLeft20Regular;
+
+  if (type === "wallet") IconComponent = Delete20Regular;
+
+  if (type === "menu-collapse") IconComponent = ArrowMinimize20Regular;
+
+  if (type === "track") IconComponent = StackStar20Regular;
+
+  if (!IconComponent) return null;
+
   return (
-    <span
-      className={
-        `lab-icon lab-icon--${type}` +
-        `${color ? ` lab-icon--${color}` : ""}` +
-        `${size ? ` lab-icon--${size}` : ""}` +
-        `${className ? ` ${className}` : ""}`
-      }
-    />
+    <span className={`${color ? ` lab-icon--${color}` : ""}`}>
+      <IconComponent />
+    </span>
   );
 }
