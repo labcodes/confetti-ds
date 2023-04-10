adcb071d8e7e78294a40d1a6939baeb563695eec
Juliana Barros Lima <judiembl@gmail.com>
2023-04-10 17:05:09 -0300

    feat: updating the Icon Button component

---
+++ apps/docs/stories/Actions/Button/IconButton.stories.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { ICON_TYPES } from "../../../../../packages/confetti-ds/src/constants";
+import { IconButton as Component } from "../../../../../packages/confetti-ds/src/IconButton";
+
+export default {
+  title: "Actions/Button/Icon Button",
+  component: Component,
+  argTypes: {
+    icon: {
+      control: { type: "select", options: ICON_TYPES },
+    },
+  },
+};
+
+export const IconButton = (args) => <Component {...args} />;
+IconButton.args = {
+  icon: "remove",
+  onClick: () => alert("Icon Button was clicked"),
+};
--- packages/confetti-ds/src/IconButton.tsx
+++ packages/confetti-ds/src/IconButton.tsx
@@ -8,6 +8,53 @@ export interface IconButtonProps {
   theme?: "teal" | "purple" | "light" | "dark";
   /** Sets the icon related to the button label. Default state: no icon. */
   icon?: IconTypes;
+  /** Disables the Button. Will be read by screen readers. When true, will override `disabled`. */
+  ariaDisabled?: boolean;
   /** Disables the Icon Button. Won't be read by screen readers. */
   disabled?: boolean;
+  /** Action to be executed when the button is clicked. */
+  onClick?: (event: SyntheticEvent) => any;
+}
+
+export default function IconButton({
+  theme = "teal",
+  icon,
+  ariaDisabled = false,
+  disabled = false,
+  onClick = (event) => {},
+}: IconButtonProps) {
+  const renderIcon = () =>
+    icon ? (
+      <Icon
+        type={icon}
+        color="white"
+        size="petit"
+        className="lab-iconbtn__icon"
+      />
+    ) : (
+      ""
+    );
+  /*
+  Set icon color to be the icon color of default button.
+  Icon color changes according to button type and exception on _buttons.scss
+  */
+  const handleOnClick = (event) => {
+    if (!isUndefined(onClick)) {
+      onClick(event);
+    }
+  };
+
+  return (
+    <span
+      className={`lab-iconbtn` + ` lab-iconbtn--${theme} lab-iconbtn--${icon}`}
+    >
+      <button
+        onClick={!ariaDisabled ? handleOnClick : () => {}}
+        disabled={(!ariaDisabled && disabled) || undefined}
+        aria-disabled={ariaDisabled || undefined}
+      >
+        {renderIcon()}
+      </button>
+    </span>
+  );
 }
