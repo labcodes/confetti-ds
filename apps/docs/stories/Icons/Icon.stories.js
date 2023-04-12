15e7b62d19a87d1a3800ce6f214a674b4fb5b071
Juliana Barros Lima <judiembl@gmail.com>
2023-04-12 14:16:41 -0300

    refactor: removing mentions to the Badge component

--- apps/docs/stories/Icons/Icon.stories.js
+++ apps/docs/stories/Icons/Icon.stories.js
@@ -1,22 +1,25 @@
 import React from "react";
 
-import { ICON_TYPES, ICON_COLORS } from "../../../../packages/confetti-ds/src/constants";
+import {
+  ICON_TYPES,
+  ICON_COLORS,
+} from "../../../../packages/confetti-ds/src/constants";
 import { Icon as Component } from "../../../../packages/confetti-ds/src";
 
 export default {
-  title: "Elements/Icon",
+  title: "Icons/Icon",
   component: Component,
   argTypes: {
     type: {
       control: { type: "select", options: ICON_TYPES },
     },
-    color:{
+    color: {
       control: { type: "select", options: ICON_COLORS },
-    }
+    },
   },
 };
 
 export const Icon = (args) => <Component {...args} />;
 Icon.args = {
-  type: "star"
+  type: "star",
 };
